5fdb47b48174cb7e74ebbe026bcff27984fd883e
metiner.kocak <metinerkck@gmail.com>
2018-06-11 22:49:14 +0300

    get api added

--- pokedex.go
+++ pokedex.go
@@ -83,36 +83,94 @@ type BaseData struct {
 }
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("/list url:", r.URL)
 
-	// If user wants to list pokemons due to their type.
-	var typeQuery = strings.ToLower(r.URL.Query().Get("type"))
+	//If user wants to list types with sorting parameters.
 	var sortQuery = strings.ToLower(r.URL.Query().Get("sortby"))
+	sortBaseData(sortQuery)
 
 
-	if(len(typeQuery) > 0){
-		if(isTypeValid(typeQuery)){
-			fmt.Println(typeQuery+" type pokemons :" )
+	//If user wants to list types
+	if(r.URL.Path == "/list/types"){
+		fmt.Println("Types:")
+		for i := 0; i < len(baseData.Types); i++ {
+			fmt.Println("\t" + baseData.Types[i].Name)
+		}
+
+
+	//If user wants to list Pokemons
+	}else if(r.URL.Path == "/list/pokemons"){
+
+
+		//If user wants to list pokemons certain type.
+		var typeQ = strings.ToLower(r.URL.Query().Get("type"))
+
+		if(len(typeQ)>0){
+			if(isTypeValid(typeQ)){
+				fmt.Println(typeQ + " type pokemons")
+				for i:=0; i<len(baseData.Pokemons);i++{
+					if(strings.ToLower(baseData.Pokemons[i].TypeI[0]) == typeQ){
+						printPokemonInfo(baseData.Pokemons[i])
+					}
+				}
+			}else{
+				fmt.Println("Is that a valid type ?")
+			}
 		}else{
-			fmt.Println("There is no type called : " + typeQuery)
+			for i:=0; i<len(baseData.Pokemons);i++{
+				printPokemonInfo(baseData.Pokemons[i])
+			}
+		}
+
+
+	//If user wants to list moves
+	}else if(r.URL.Path == "/list/moves"){
+
+		fmt.Println("Moves:")
+		for i := 0; i < len(baseData.Moves); i++ {
+			fmt.Println("\t" + baseData.Moves[i].Name)
 		}
 	}
-
-	if(r.URL.Path == "/list/types"){
-		fmt.Println("Pokemon Types:")
-	}
-
-
-	printPokemonInfo(typeQuery,sortQuery)
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("/get url:", r.URL)
-	fmt.Fprint(w, "The Get Handler\n")
+
+	//If user wants to get certain pokemon
+	if(r.URL.Path == "/get/pokemon"){
+
+		var pokemonName = strings.ToLower(r.URL.Query().Get("name"))
+		for i := 0; i < len(baseData.Pokemons); i++ {
+			if(strings.ToLower(baseData.Pokemons[i].Name) == pokemonName){
+				printPokemonInfo(baseData.Pokemons[i])
+			}
+		}
+	}
+	//If user wants to get certain move
+	if(r.URL.Path == "/get/move"){
+
+		var moveName = strings.ToLower(r.URL.Query().Get("name"))
+		for i := 0; i < len(baseData.Moves); i++ {
+			if(strings.ToLower(baseData.Moves[i].Name) == moveName){
+				printMoveInfo(baseData.Moves[i])
+			}
+		}
+	}
+	//If user wants to get certain type
+	if(r.URL.Path == "/get/type"){
+		var typeName = strings.ToLower(r.URL.Query().Get("name"))
+		if(isTypeValid(typeName)){
+			for i := 0; i < len(baseData.Types); i++ {
+				if(strings.ToLower(baseData.Types[i].Name) == typeName){
+					printTypeInfo(baseData.Types[i])
+				}
+			}
+		}else{
+			fmt.Println("Is that a valid type ?")
+		}
+	}
 }
 
 func otherwise(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World\n")
+	fmt.Fprint(w, "Oops, there is no such a thing :(")
 }
 
 
@@ -137,71 +195,113 @@ func FloatToString(input_num float64) string {
 	return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 
-func printPokemonInfo(queryParam string,sortBy string) {
+func sortBaseData(sortBy string){
+	sortBy = strings.ToLower(sortBy)
 
 	if(len(sortBy) > 0){
 		fmt.Println("Sorted by: " + sortBy)
 		if(sortBy == "baseattack"){
 			sort.Slice(baseData.Pokemons[:], func(i, j int) bool {
-				return baseData.Pokemons[i].BaseAttack < baseData.Pokemons[j].BaseAttack
+				return baseData.Pokemons[i].BaseAttack > baseData.Pokemons[j].BaseAttack
 			})
 		}else if(sortBy == "basedefense"){
 			sort.Slice(baseData.Pokemons[:], func(i, j int) bool {
-				return baseData.Pokemons[i].BaseDefense < baseData.Pokemons[j].BaseDefense
+				return baseData.Pokemons[i].BaseDefense > baseData.Pokemons[j].BaseDefense
 			})
 		}else if(sortBy == "basestamina"){
 			sort.Slice(baseData.Pokemons[:], func(i, j int) bool {
-				return baseData.Pokemons[i].BaseStamina < baseData.Pokemons[j].BaseStamina
+				return baseData.Pokemons[i].BaseStamina > baseData.Pokemons[j].BaseStamina
+			})
+		}else if(sortBy == "damage"){
+			sort.Slice(baseData.Moves[:], func(i, j int) bool {
+				return baseData.Moves[i].Damage > baseData.Moves[j].Damage
+			})
+		}else if(sortBy == "energy"){
+			sort.Slice(baseData.Moves[:], func(i, j int) bool {
+				return baseData.Moves[i].Energy > baseData.Moves[j].Energy
+			})
+		}else if(sortBy == "dps"){
+			sort.Slice(baseData.Moves[:], func(i, j int) bool {
+				return baseData.Moves[i].Dps > baseData.Moves[j].Dps
+			})
+		}else if(sortBy == "duration"){
+			sort.Slice(baseData.Moves[:], func(i, j int) bool {
+				return baseData.Moves[i].Duration > baseData.Moves[j].Duration
 			})
 		}
 
 	}
 
-	for i := 0; i < len(baseData.Pokemons); i++ {
-		if(strings.ToLower(baseData.Pokemons[i].TypeI[0]) == queryParam){
+}
+func printPokemonInfo(pokemon Pokemon) {
 
-			fmt.Println(baseData.Pokemons[i].Name+":" )
-			fmt.Println("\t Type I: " + baseData.Pokemons[i].TypeI[0])
-			if(baseData.Pokemons[i].TypeII != nil){
-				fmt.Println("\t Type II: " + baseData.Pokemons[i].TypeII[0])
-			}
-			fmt.Println("\t Height: " + baseData.Pokemons[i].Height)
-			fmt.Println("\t Weight: " + baseData.Pokemons[i].Weight)
-			fmt.Println("\t Base Attack: " + strconv.Itoa(baseData.Pokemons[i].BaseAttack))
-			fmt.Println("\t Base Defense: " + strconv.Itoa(baseData.Pokemons[i].BaseDefense))
-			fmt.Println("\t Base Stamina: " + strconv.Itoa(baseData.Pokemons[i].BaseStamina))
-			fmt.Println("\t Buddy Distance Needed: " + strconv.Itoa(baseData.Pokemons[i].BuddyDistanceNeeded))
-			fmt.Println("\t Candy Name: " + baseData.Pokemons[i].Candy.Name)
-			fmt.Println("\t CaptureRate: " + FloatToString(baseData.Pokemons[i].CaptureRate))
-			//fmt.Println("\t Classification: " + baseData.Pokemons[i].Classification)
-			fmt.Println("\t FastAttackS: ")
-			for j := 0; j < len(baseData.Pokemons[i].FastAttackS);j++{
-				fmt.Println("\t\t " + baseData.Pokemons[i].FastAttackS[j])
-			}
-			fmt.Println("\t FleeRate: " + FloatToString(baseData.Pokemons[i].FleeRate))
-			fmt.Println("\t Weaknesses: ")
-			for k := 0; k < len(baseData.Pokemons[i].Weaknesses);k++{
-				fmt.Println("\t\t " + baseData.Pokemons[i].Weaknesses[k])
-			}
-			fmt.Println("\t Next Evolution Requirements: " )
-			fmt.Println("\t\t Name: " + baseData.Pokemons[i].NextEvolutionRequirements.Name)
-			fmt.Println("\t\t Amount: " + strconv.Itoa(baseData.Pokemons[i].NextEvolutionRequirements.Amount))
+	fmt.Println(pokemon.Name+":" )
+	fmt.Println("\t Type I: " + pokemon.TypeI[0])
+	if(pokemon.TypeII != nil){
+		fmt.Println("\t Type II: " + pokemon.TypeII[0])
+	}
+	fmt.Println("\t Height: " + pokemon.Height)
+	fmt.Println("\t Weight: " + pokemon.Weight)
+	fmt.Println("\t Base Attack: " + strconv.Itoa(pokemon.BaseAttack))
+	fmt.Println("\t Base Defense: " + strconv.Itoa(pokemon.BaseDefense))
+	fmt.Println("\t Base Stamina: " + strconv.Itoa(pokemon.BaseStamina))
+	fmt.Println("\t Buddy Distance Needed: " + strconv.Itoa(pokemon.BuddyDistanceNeeded))
+	fmt.Println("\t Candy Name: " + pokemon.Candy.Name)
+	fmt.Println("\t CaptureRate: " + FloatToString(pokemon.CaptureRate))
+	//fmt.Println("\t Classification: " + baseData.Pokemons[i].Classification)
+	fmt.Println("\t FastAttackS: ")
+	for j := 0; j < len(pokemon.FastAttackS);j++{
+		fmt.Println("\t\t " + pokemon.FastAttackS[j])
+	}
+	fmt.Println("\t FleeRate: " + FloatToString(pokemon.FleeRate))
+	fmt.Println("\t Weaknesses: ")
+	for k := 0; k < len(pokemon.Weaknesses);k++{
+		fmt.Println("\t\t " + pokemon.Weaknesses[k])
+	}
+	fmt.Println("\t Next Evolution Requirements: " )
+	fmt.Println("\t\t Name: " + pokemon.NextEvolutionRequirements.Name)
+	fmt.Println("\t\t Amount: " + strconv.Itoa(pokemon.NextEvolutionRequirements.Amount))
 
-			if(baseData.Pokemons[i].NextEvolutions != nil){
-				fmt.Println("\t Next Evolutions: ")
-				for l := 0; l < len(baseData.Pokemons[i].NextEvolutions);l++{
-					fmt.Println("\t\t Name: " + string(baseData.Pokemons[i].NextEvolutions[l].Name))
-				}
-			}
-			if(baseData.Pokemons[i].PreviousEvolutions != nil){
-				fmt.Println("\t Previous Evolutions: ")
-				for l := 0; l < len(baseData.Pokemons[i].PreviousEvolutions);l++{
-					fmt.Println("\t\t Name: " + string(baseData.Pokemons[i].PreviousEvolutions[l].Name))
-				}
-			}
+	if(pokemon.NextEvolutions != nil){
+		fmt.Println("\t Next Evolutions: ")
+		for l := 0; l < len(pokemon.NextEvolutions);l++{
+			fmt.Println("\t\t Name: " + string(pokemon.NextEvolutions[l].Name))
+		}
+	}
+	if(pokemon.PreviousEvolutions != nil){
+		fmt.Println("\t Previous Evolutions: ")
+		for l := 0; l < len(pokemon.PreviousEvolutions);l++{
+			fmt.Println("\t\t Name: " + string(pokemon.PreviousEvolutions[l].Name))
 		}
 	}
 }
+func printMoveInfo(move Move) {
+
+	fmt.Println(move.Name+":" )
+
+	fmt.Println("\t Type: " + move.Type)
+	fmt.Println("\t Damage: " + strconv.Itoa(move.Damage))
+	fmt.Println("\t Duration: " + strconv.Itoa(move.Duration))
+	fmt.Println("\t Dps: " + FloatToString(move.Dps))
+	fmt.Println("\t Energy: " + strconv.Itoa(move.Energy))
+
+}
+func printTypeInfo(typ Type) {
+
+	fmt.Println(typ.Name+":" )
+
+	fmt.Println("\t Effective Against: ")
+	for l := 0; l < len(typ.EffectiveAgainst);l++{
+		fmt.Println("\t\t: " + typ.EffectiveAgainst[l])
+	}
+	fmt.Println("\t Weak Against: ")
+	for l := 0; l < len(typ.WeakAgainst);l++{
+		fmt.Println("\t\t: " + typ.WeakAgainst[l])
+	}
+
+
+}
+
 func isTypeValid(queryParam string) bool{
 	for i := 0; i<len(baseData.Types);i++{
 		if(queryParam == strings.ToLower(baseData.Types[i].Name)){
@@ -216,12 +316,17 @@ func main() {
 	//TODO: read data.json to a BaseData
 	baseData  = jsonReader()
 
-	http.HandleFunc("/list", listHandler)
-	http.HandleFunc("/get", getHandler)
+
+	//Get handlers
+	http.HandleFunc("/get/type", getHandler)
+	http.HandleFunc("/get/pokemon", getHandler)
+	http.HandleFunc("/get/move", getHandler)
 
 
 	//List handlers
 	http.HandleFunc("/list/types", listHandler)
+	http.HandleFunc("/list/pokemons", listHandler)
+	http.HandleFunc("/list/moves", listHandler)
 
 
 	//TODO: add more
